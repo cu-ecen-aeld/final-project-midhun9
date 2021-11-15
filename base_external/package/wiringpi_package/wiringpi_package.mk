4f82ee8a7bb44aabeaaff00074af0f095e350e2c
midhun9 <midhun.koneru111@gmail.com>
2021-11-14 20:51:47 -0700

    Updated

--- base_external/package/wiringpi_package/wiringpi_package.mk
+++ base_external/package/wiringpi_package/wiringpi_package.mk
@@ -1,26 +1,27 @@
 
 ##############################################################
 #
-# GPIO-PACKAGE
+# WIRINGPI-PACKAGE
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-GPIO_PACKAGE_VERSION = '99c3632451b6d059741220dafc8e10822bd2ef55'
+WIRINGPI_PACKAGE_VERSION = '7f8fe26e4f775abfced43c07657a353f03ddb2d0'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-GPIO_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
-GPIO_PACKAGE_SITE_METHOD = git
-GPIO_PACKAGE_GIT_SUBMODULES = YES
+WIRINGPI_PACKAGE_SITE = 'git@github.com:WiringPi/WiringPi.git'
+WIRINGPI_PACKAGE_SITE_METHOD = git
+WIRINGPI_PACKAGE_GIT_SUBMODULES = YES
 
-define GPIO_PACKAGE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
+define WIRINGPI_PACKAGE_BUILD_CMDS
+	cd /home/midhun/Desktop/aesd/final-project-midhun9/buildroot/output/build/wiringpi_package-7f8fe26e4f775abfced43c07657a353f03ddb2d0/
+	sh build
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define GPIO_PACKAGE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
-endef
+#define WIRINGPIPACKAGE_INSTALL_TARGET_CMDS
+#	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
+#endef
 
 $(eval $(generic-package))
