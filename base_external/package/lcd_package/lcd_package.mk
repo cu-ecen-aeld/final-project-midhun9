6e5f6d5b738ed74d3156564ceae067d647bb98d7
midhun9 <midhun.koneru111@gmail.com>
2021-11-21 20:28:11 -0700

    updated

--- base_external/package/lcd_package/lcd_package.mk
+++ base_external/package/lcd_package/lcd_package.mk
@@ -1,26 +1,26 @@
 
 ##############################################################
 #
-# GPIO-PACKAGE
+# LCD-PACKAGE
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-GPIO_PACKAGE_VERSION = '5ea7b7f3c32d900169aaef73152b0542b732be9e'
+LCD_PACKAGE_VERSION = 'b4210067b5520a4aed91590fba02116ebe580a40'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-GPIO_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
-GPIO_PACKAGE_SITE_METHOD = git
-GPIO_PACKAGE_GIT_SUBMODULES = YES
+LCD_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
+LCD_PACKAGE_SITE_METHOD = git
+LCD_PACKAGE_GIT_SUBMODULES = YES
 
-define GPIO_PACKAGE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
+define LCD_PACKAGE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define GPIO_PACKAGE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
+define LCD_PACKAGE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/lcd/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- base_external/package/socket-test_package/socket-test_package.mk
+++ base_external/package/socket-test_package/socket-test_package.mk
@@ -1,26 +1,26 @@
 
 ##############################################################
 #
-# GPIO-PACKAGE
+# SOCKET-TEST-PACKAGE
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-GPIO_PACKAGE_VERSION = '5ea7b7f3c32d900169aaef73152b0542b732be9e'
+SOCKET_TEST_PACKAGE_VERSION = '402a43381e3bf601c5a7703fc22038bee3834986'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-GPIO_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
-GPIO_PACKAGE_SITE_METHOD = git
-GPIO_PACKAGE_GIT_SUBMODULES = YES
+SOCKET_TEST_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
+SOCKET_TEST_PACKAGE_SITE_METHOD = git
+SOCKET_TEST_PACKAGE_GIT_SUBMODULES = YES
 
-define GPIO_PACKAGE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
+define SOCKET_TEST_PACKAGE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/socket_test all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define GPIO_PACKAGE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
+define SOCKET_TEST_PACKAGE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/socket_test/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
