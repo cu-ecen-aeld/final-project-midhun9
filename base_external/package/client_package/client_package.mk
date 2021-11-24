81fea191f7d8399e3f45987f6eb0f327e13c24d5
midhun9 <midhun.koneru111@gmail.com>
2021-11-23 19:42:24 -0700

    Client package added

--- base_external/package/client_package/client_package.mk
+++ base_external/package/client_package/client_package.mk
@@ -1,26 +1,26 @@
 
 ##############################################################
 #
-# GPIO-PACKAGE
+# CLIENT-PACKAGE
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-GPIO_PACKAGE_VERSION = '3867ab3b0a63fbc2b9e6957f7c0015166805cc0f'
+CLIENT_PACKAGE_VERSION = '820e1500509dd933e44f6a2efde10872e492a918'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-GPIO_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
-GPIO_PACKAGE_SITE_METHOD = git
-GPIO_PACKAGE_GIT_SUBMODULES = YES
+CLIENT_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
+CLIENT_PACKAGE_SITE_METHOD = git
+CLIENT_PACKAGE_GIT_SUBMODULES = YES
 
-define GPIO_PACKAGE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
+define CLIENT_PACKAGE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define GPIO_PACKAGE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
+define CLIENT_PACKAGE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/client/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- base_external/package/lcd16x2_package/lcd16x2_package.mk
+++ base_external/package/lcd16x2_package/lcd16x2_package.mk
@@ -1,26 +1,26 @@
 
 ##############################################################
 #
-# GPIO-PACKAGE
+# LCD-PACKAGE
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-GPIO_PACKAGE_VERSION = '3867ab3b0a63fbc2b9e6957f7c0015166805cc0f'
+LCD16X2_PACKAGE_VERSION = '3867ab3b0a63fbc2b9e6957f7c0015166805cc0f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-GPIO_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
-GPIO_PACKAGE_SITE_METHOD = git
-GPIO_PACKAGE_GIT_SUBMODULES = YES
+LCD16X2_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-support-midhun-venkat.git'
+LCD16X2_PACKAGE_SITE_METHOD = git
+LCD16X2_PACKAGE_GIT_SUBMODULES = YES
 
-define GPIO_PACKAGE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
+define LCD16X2_PACKAGE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lcd16x2 all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define GPIO_PACKAGE_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/gpio/* $(TARGET_DIR)/usr/bin
+define LCD16X2_PACKAGE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/lcd16x2/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
